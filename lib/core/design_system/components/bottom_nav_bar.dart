0d0aa2c02a321076647ffcdb36bafbde48f51508
Abd2lhakam19 <midoahme93@gmail.com>
2025-12-24 21:15:51 +0200

    implement BottomNavBar component

---
+++ lib/core/design_system/components/bottom_nav_bar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:hr_management/core/design_system/theme/helper/extention_colors.dart';
+import 'package:hr_management/core/design_system/theme/helper/theme_extention.dart';
+
+import 'package:iconsax/iconsax.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int index;
+  final Function(int) onTap;
+  const BottomNavBar({super.key, required this.index, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = context.colors;
+
+    final List<IconData> notSelectedIcons = [
+      Iconsax.home,
+      Iconsax.calendar_2,
+      Iconsax.note_text,
+      Iconsax.receipt_2,
+      Iconsax.layer,
+    ];
+    final List<IconData> selectedIcons = [
+      Iconsax.home_15,
+      Iconsax.calendar_25,
+      Iconsax.note_text5,
+      Iconsax.receipt_21,
+      Iconsax.layer5,
+    ];
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      width: double.infinity,
+      constraints: const BoxConstraints(maxHeight: 75),
+      decoration: BoxDecoration(color: ExtentionColors.bottomNavBarBgColor),
+      child: Row(
+        mainAxisAlignment: .spaceBetween,
+        children: List.generate(selectedIcons.length, (idx) {
+          bool isSelected = index == idx;
+          return InkWell(
+            onTap: () => onTap(idx),
+            child: Column(
+              children: [
+                Icon(
+                  isSelected ? selectedIcons[idx] : notSelectedIcons[idx],
+                  color: color.pureWhite,
+                ),
+                const SizedBox(height: 5),
+                AnimatedOpacity(
+                  duration: Duration(milliseconds: 200),
+                  opacity: isSelected ? 1.0 : 0.0,
+                  child: Container(
+                    width: 12,
+                    height: 2,
+                    decoration: BoxDecoration(
+                      color: color.pureWhite,
+                      borderRadius: BorderRadius.circular(2),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
--- lib/core/design_system/theme/helper/extention_colors.dart
+++ lib/core/design_system/theme/helper/extention_colors.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hr_management/core/design_system/theme/color/app_constant_colors.dart';
 
-
-class ExtentionColors{
-  static const prupleProfile= Color(0xff675aff);
-  static const blackTitleProfile=Color(0xff2D2D2D);
+class ExtentionColors {
+  static const prupleProfile = Color(0xff675aff);
+  static const blackTitleProfile = Color(0xff2D2D2D);
   static const Color iconDefault = AppConstantColors.purple500;
   static const Color backgroundContainer = Color(0xFFF9FAFB);
   static const Color borderContainer = Color(0xFFEAECF0);
@@ -12,4 +11,5 @@ class ExtentionColors{
   static const Color okay = Color(0xFFFFC02E);
   static const Color poor = Color(0xFFFD824C);
   static const Color bad = Color(0xFFF95555);
+  static const Color bottomNavBarBgColor = Color(0xff1C2020);
 }
