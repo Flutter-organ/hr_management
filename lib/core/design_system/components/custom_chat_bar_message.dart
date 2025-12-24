ea5ca2a178d2b4f8823b86d94426f34643d283da
emad2003-flutter <emadmahgoub09@gmail.com>
2025-12-24 23:58:47 +0200

    feat(message chat bar): add custom chat input bar with send/mic toggle

---
+++ lib/core/design_system/components/custom_chat_bar_message.dart
@@ -0,0 +1,121 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hr_management/core/design_system/theme/helper/app_assets.dart';
+import 'package:hr_management/core/design_system/theme/helper/extention_colors.dart';
+import 'package:hr_management/core/design_system/theme/helper/theme_extention.dart';
+
+class CustomChatBarMessage extends StatelessWidget {
+  final TextEditingController messageController;
+  final VoidCallback? onSend;
+  final VoidCallback? onAttachTap;
+  final VoidCallback? onCameraTap;
+
+  const CustomChatBarMessage({
+    super.key,
+    required this.messageController,
+    this.onSend,
+    this.onAttachTap,
+    this.onCameraTap,
+  });
+
+  bool _hasText() {
+    return messageController.text.trim().isNotEmpty;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: context.colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+
+      child: Row(
+        children: [
+          /// Text Field + Attach + Camera
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: context.colors.gray50,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                children: [
+                  /// Text Field
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: TextFormField(
+                        controller: messageController,
+                        style: TextStyle(
+                          color: ExtentionColors.blackTitleProfile,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "type_a_message...".tr(),
+                          hintStyle: TextStyle(
+                            color: ExtentionColors.blackTitleProfile,
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 12,
+                          ),
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (_) {
+                          // to rebuild the widget to update the send/mic button state
+                          (context as Element).markNeedsBuild();
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+
+                  /// Attach
+                  InkWell(
+                    onTap: onAttachTap,
+                    child: SvgPicture.asset(
+                      AppAssets.kAttach,
+                      color: ExtentionColors.blackTitleProfile,
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+
+                  /// Camera
+                  InkWell(
+                    onTap: onCameraTap,
+                    child: SvgPicture.asset(
+                      AppAssets.kCamera,
+                      color: ExtentionColors.blackTitleProfile,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
+
+          /// Send / Mic Button
+          SizedBox(
+            width: 56,
+            height: 56,
+            child: GestureDetector(
+              onTap: _hasText() ? onSend : null,
+              child: CircleAvatar(
+                backgroundColor: ExtentionColors.iconDefault,
+                child: SvgPicture.asset(
+                  _hasText() ? AppAssets.kSend : AppAssets.kMic,
+                  color: context.colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/core/design_system/theme/helper/app_assets.dart
+++ lib/core/design_system/theme/helper/app_assets.dart
@@ -1,12 +1,16 @@
 class AppAssets {
   //example
   // general
-  static const  kProperty = 'assets/svgs/Property.svg';
+  static const kProperty = 'assets/svgs/Property.svg';
+  static const kAttach = 'assets/svgs/Attach.svg';
+  static const kSend = 'assets/svgs/send.svg';
+  static const kMic = 'assets/svgs/mic.svg';
+  static const kCamera = 'assets/svgs/camera.svg';
   // static const String emptyState = 'assets/images/empty_state.png';
-  static const  goodEmoji = 'assets/images/good.png';
+  static const goodEmoji = 'assets/images/good.png';
   static const okayEmoji = 'assets/images/okay.png';
   static const poorEmoji = 'assets/images/poor.png';
   static const badEmoji = 'assets/images/bad.png';
-  static const  placeHolderProfile = 'assets/images/placeholder_profile.png';
-  static const  profilePlaceholder = 'assets/images/profile.png';
+  static const placeHolderProfile = 'assets/images/placeholder_profile.png';
+  static const profilePlaceholder = 'assets/images/profile.png';
 }
