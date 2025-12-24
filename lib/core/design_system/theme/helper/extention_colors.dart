aace7e7758bac6a7e3999e61c74147e4d9dcc686
elsankry02 <githup800900@gmail.com>
2025-12-24 10:29:20 +0200

    feat: create custom_user_tile and display it to test

--- lib/core/design_system/theme/helper/extention_colors.dart
+++ lib/core/design_system/theme/helper/extention_colors.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hr_management/core/design_system/theme/color/app_constant_colors.dart';
 
 class ExtensionColors {
-  static const prupleProfile= Color(0xff675aff);
-  static const blackTitleProfile=Color(0xff2D2D2D);
+  static const prupleProfile = Color(0xff675aff);
+  static const blackTitleProfile = Color(0xff2D2D2D);
   static const Color iconDefault = AppConstantColors.purple500;
   static const Color backgroundContainer = Color(0xFFF9FAFB);
   static const Color borderContainer = Color(0xFFEAECF0);
@@ -15,4 +15,5 @@ class ExtensionColors {
   static const Color okay = Color(0xFFFFC02E);
   static const Color poor = Color(0xFFFD824C);
   static const Color bad = Color(0xFFF95555);
+  static const Color kOne = Color(0xFFF54975);
 }
