fdc8fe38e2b9ff8ae9390fc5baa13cc5fe98b292
elsankry02 <githup800900@gmail.com>
2025-12-25 20:37:32 +0200

    fix: fix conflicts

--- lib/core/design_system/theme/helper/extention_colors.dart
+++ lib/core/design_system/theme/helper/extention_colors.dart
@@ -4,25 +4,21 @@ import 'package:hr_management/core/design_system/theme/color/app_constant_colors
 class ExtensionColors {
   static const prupleProfile = Color(0xff675aff);
   static const blackTitleProfile = Color(0xff2D2D2D);
-  static const Color iconDefault = AppConstantColors.purple500;context.colors.purple100
+  static const Color iconDefault = AppConstantColors.purple500;
   static const Color backgroundContainer = Color(0xFFF9FAFB);
   static const Color borderContainer = Color(0xFFEAECF0);
   static const Color textPrimary = Color(0xFF101828);
   static const Color textSecondary = Color(0xFF475467);
   static const Color cardBackground = Color(0xFFF9F9F9);
-
   static const Color good = Color(0xFF47C28B);
   static const Color okay = Color(0xFFFFC02E);
   static const Color poor = Color(0xFFFD824C);
   static const Color bad = Color(0xFFF95555);
-<<<<<<< HEAD
   static const Color kTwo = Color(0xFF7A5AF8);
-=======
   static const Color bottomNavBarBgColor = Color(0xff1C2020);
   static const grayCheckBox = Color(0xFF98A2B3);
   static const grayText = Color(0xFF2D2D2D);
   static const transparentColor = Colors.transparent;
-
   static const Color backgroundCard = Color(0xFFFEFEFE);
   static const Color red500 = Color(0xFFF95555);
   static const Color yellow500 = Color(0xFFFFC02E);
@@ -32,12 +28,4 @@ class ExtensionColors {
   static const Color gray600 = Color(0xFF71717A);
   static const Color gray200 = Color(0xFFE2E2E5);
   static const Color grayStatusBar = Color(0xFFE7E7E7);
-
-
-
-
-
-
-
->>>>>>> system_design
 }
