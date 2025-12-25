3e2e03d8b11c69903c1d632a974474daa8ccacb4
Abd2lhakam19 <midoahme93@gmail.com>
2025-12-25 17:24:52 +0200

    feat: refactor BottomNavBar to use default BottomNavigationBar

--- lib/core/design_system/components/bottom_nav_bar.dart
+++ lib/core/design_system/components/bottom_nav_bar.dart
@@ -1,68 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:hr_management/core/design_system/theme/helper/extention_colors.dart';
 import 'package:hr_management/core/design_system/theme/helper/theme_extention.dart';
-
 import 'package:iconsax/iconsax.dart';
 
 class BottomNavBar extends StatelessWidget {
-  final int index;
+  BottomNavBar({super.key, required this.index, required this.onTap});
   final Function(int) onTap;
-  const BottomNavBar({super.key, required this.index, required this.onTap});
+  final int index;
+  static final List<IconModel> icons = [
+    IconModel(icon: Iconsax.home, activeIcon: Iconsax.home_15),
+    IconModel(icon: Iconsax.calendar_2, activeIcon: Iconsax.calendar_25),
+    IconModel(icon: Iconsax.note_text, activeIcon: Iconsax.note_text5),
+    IconModel(icon: Iconsax.receipt_2, activeIcon: Iconsax.receipt_25),
+    IconModel(icon: Iconsax.layer, activeIcon: Iconsax.layer5),
+  ];
 
   @override
   Widget build(BuildContext context) {
     final color = context.colors;
-
-    final List<IconData> notSelectedIcons = [
-      Iconsax.home,
-      Iconsax.calendar_2,
-      Iconsax.note_text,
-      Iconsax.receipt_2,
-      Iconsax.layer,
-    ];
-    final List<IconData> selectedIcons = [
-      Iconsax.home_15,
-      Iconsax.calendar_25,
-      Iconsax.note_text5,
-      Iconsax.receipt_21,
-      Iconsax.layer5,
-    ];
-
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-      width: double.infinity,
-      constraints: const BoxConstraints(maxHeight: 75),
-      decoration: BoxDecoration(color: ExtensionColors.bottomNavBarBgColor),
-      child: Row(
-        mainAxisAlignment: .spaceBetween,
-        children: List.generate(selectedIcons.length, (idx) {
-          bool isSelected = index == idx;
-          return InkWell(
-            onTap: () => onTap(idx),
-            child: Column(
-              children: [
-                Icon(
-                  isSelected ? selectedIcons[idx] : notSelectedIcons[idx],
-                  color: color.pureWhite,
-                ),
-                const SizedBox(height: 5),
-                AnimatedOpacity(
-                  duration: Duration(milliseconds: 200),
-                  opacity: isSelected ? 1.0 : 0.0,
-                  child: Container(
-                    width: 12,
-                    height: 2,
-                    decoration: BoxDecoration(
-                      color: color.pureWhite,
-                      borderRadius: BorderRadius.circular(2),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        }),
+    final items = List.generate(5, (index) => (index));
+    return Theme(
+      data: Theme.of(context).copyWith(
+        splashColor: ExtensionColors.transparentColor,
+        highlightColor: ExtensionColors.transparentColor,
+      ),
+      child: BottomNavigationBar(
+        backgroundColor: ExtensionColors.bottomNavBarBgColor,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: index,
+        onTap: (value) => onTap(value),
+        selectedItemColor: color.pureWhite,
+        unselectedItemColor: color.pureWhite,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 0,
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+        items: items.map((item) {
+          return _buildBottomNavBarItem(icons[item], color, "");
+        }).toList(),
       ),
     );
   }
+
+  BottomNavigationBarItem _buildBottomNavBarItem(
+    IconModel icons,
+    dynamic color,
+    String label,
+  ) {
+    return BottomNavigationBarItem(
+      icon: Icon(icons.icon),
+      activeIcon: Column(
+        mainAxisSize: .min,
+        children: [
+          Icon(icons.activeIcon),
+          const SizedBox(height: 8),
+          Container(
+            width: 12,
+            height: 2,
+            decoration: BoxDecoration(
+              color: color.pureWhite,
+              borderRadius: BorderRadius.circular(2),
+            ),
+          ),
+        ],
+      ),
+      label: label,
+    );
+  }
+}
+
+class IconModel {
+  IconData icon, activeIcon;
+  IconModel({required this.icon, required this.activeIcon});
 }
