5d1424c5ec32876e9a36584ea04986e3e9b4d942
norhanadel367 <norhanadel367@gmail.com>
2025-12-28 19:46:49 +0200

    feat : fix primary button

--- lib/core/design_system/components/custom_primary_button.dart
+++ lib/core/design_system/components/custom_primary_button.dart
@@ -1,69 +1,150 @@
 import 'package:flutter/material.dart';
 
+import '../theme/helper/extention_colors.dart';
+
 class CustomPrimaryButton extends StatelessWidget {
-  final String title;
-  final BoxBorder? border;
-  final double? spaceBetween, width;
-  final TextAlign? textAlign;
-  final Color backGroundColor;
-  final Color? circularColor;
-  final EdgeInsetsGeometry? margin, padding;
-  final BorderRadius? borderRadius;
-  final Gradient? gradient;
-  final TextStyle? style;
+  const CustomPrimaryButton(
+      {
+        Key? key,
+        required this.buttonText,
+        required this.onPressedFunction,
+        this.buttonBackgroundColor,
+        this.onPressedColor,
+        this.shadowBackgroundColor,
+        this.elevation = 0.0,
+        this.width = 343,
+        this.height = 48,
+        this.iconColor = (ExtensionColors.grayStatusBar),
+        this.buttonTextStyle = const TextStyle(
+            color: ExtensionColors.whiteContainer
+        ),
+        this.icon,
+        this.radius = 8,
+        this.spaceSize = 8.0,
+        this.boarderWidth = 1.0,
+        this.isLoading = false,
+        this.isEnable = true,
+        this.showBorder = false,
+        this.borderColor = ExtensionColors.whiteContainer, required this.showTextDuringLoading})
+      : super(key: key);
+  final String buttonText;
+  final Function() onPressedFunction;
+  final Color? buttonBackgroundColor;
+  final Color? onPressedColor;
+  final Color? shadowBackgroundColor;
+  final double? elevation;
+  final double? width;
+  final double height;
+  final TextStyle? buttonTextStyle;
+  final Color? iconColor;
+  final double? radius;
+  final Widget? icon;
+  final double? spaceSize;
+  final double? boarderWidth;
   final bool isLoading;
-  final void Function()? onTap;
-  const CustomPrimaryButton({
-    super.key,
-    required this.title,
-    required this.backGroundColor,
-    this.margin,
-    this.gradient,
-    this.style,
-    this.onTap,
-    this.padding,
-    this.isLoading = false,
-    this.border,
-    this.borderRadius,
-    this.textAlign = TextAlign.center,
-    this.circularColor,
-    this.spaceBetween,
-    this.width,
-  });
+  final bool isEnable;
+  final bool showBorder;
+  final Color? borderColor;
+  final bool showTextDuringLoading;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      borderRadius: borderRadius,
-      onTap: isLoading ? null : onTap,
-      child: Container(
-        width: width,
-        margin: margin,
-        padding: padding,
-        decoration: BoxDecoration(
-          color: backGroundColor,
-          border: border,
-          borderRadius: borderRadius,
-          gradient: gradient,
+    // double getWidgetHeight(double height) {
+    //   final double currentHeight = MediaQuery.heightOf(context) * (height / 812);
+    //   return currentHeight;
+    // }
+    //
+    // double getWidgetWidth(double width) {
+    //   final double currentWidth = MediaQuery.widthOf(context) * (width / 375);
+    //   return currentWidth;
+    // }
+    // final double buttonWidth = getWidgetWidth(width!);
+    // final double buttonHeight = getWidgetHeight(height!);
+    return  SizedBox(
+      width: width ?? double.infinity,
+      height: height ,
+      child: ElevatedButton(
+          onPressed: isLoading || !isEnable ? null : onPressedFunction,
+          style: ButtonStyle(
+            elevation: WidgetStateProperty.all<double>(elevation!),
+            side: WidgetStateProperty.all<BorderSide>(
+              BorderSide(
+                color: showBorder
+                    ? isLoading || !isEnable
+                    ? ExtensionColors.yellow50
+                    : borderColor!
+                    : ExtensionColors.green50,
+                width: boarderWidth ?? 1,
+              ),
+            ),
+            shadowColor: WidgetStateProperty.all(
+              shadowBackgroundColor ?? ExtensionColors.green50.withOpacity(.3),
+            ),
+            overlayColor: WidgetStateProperty.all(
+              onPressedColor ?? ExtensionColors.red500.withOpacity(.25),
+            ),
+            shape: WidgetStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(radius!),
+              ),
+            ),
+            fixedSize:
+            WidgetStateProperty.all<Size>(Size(width ?? double.infinity,height )),
+            backgroundColor: isLoading || !isEnable
+                ? WidgetStateProperty.all(ExtensionColors.poor)
+                : WidgetStateProperty.all(
+                buttonBackgroundColor ?? ExtensionColors.gray200),
+          ),
+            child: Center(
+              child: isLoading
+                  ? _buildLoadingContent()
+                  : _buildNormalContent(),
+            )
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            isLoading
-                ? SizedBox(
-                    height: 20,
-                    width: 20,
-                    child: CircularProgressIndicator(
-                      color: circularColor,
-                      strokeWidth: 1.5,
-                    ),
-                  )
-                : SizedBox(),
-            SizedBox(width: isLoading ? spaceBetween : 0),
-            Text(title, textAlign: textAlign, style: style),
-          ],
+    );
+  }
+  Widget _buildNormalContent() {
+    if (icon == null) {
+      return Text(
+        buttonText,
+        style: buttonTextStyle,
+      );
+    }
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        icon!,
+        SizedBox(width: spaceSize),
+        Text(
+          buttonText,
+          style: buttonTextStyle,
         ),
-      ),
+      ],
+    );
+  }
+
+  Widget _buildLoadingContent() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(
+          width: 18,
+          height: 18,
+          child: CircularProgressIndicator(
+            strokeWidth: 2.4,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          ),
+        ),
+        if (showTextDuringLoading) ...[
+          const SizedBox(width: 12),
+          Text(
+            buttonText,
+            style: buttonTextStyle
+          ),
+        ],
+      ],
     );
   }
 }
