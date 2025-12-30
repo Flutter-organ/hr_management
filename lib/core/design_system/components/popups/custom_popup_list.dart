51e1cbf9f8c3c3a6a6175ab709012933aaef0a89
MohamedMahmoudAli <mohamed238ma@gmail.com>
2025-12-30 15:18:50 +0200

    edit the design of custom pop list

--- lib/core/design_system/components/popups/custom_popup_list.dart
+++ lib/core/design_system/components/popups/custom_popup_list.dart
@@ -2,7 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hr_management/core/design_system/components/custom_primary_button.dart';
 import 'package:hr_management/core/design_system/components/selectable_chip.dart';
-import 'package:hr_management/core/design_system/theme/color/app_constant_colors.dart';
 import 'package:hr_management/core/design_system/theme/helper/theme_extention.dart';
 
 class CustomPopupList extends StatefulWidget {
@@ -10,91 +9,106 @@ class CustomPopupList extends StatefulWidget {
     super.key,
     required this.title,
     required this.subtitle,
-    required this.options,  this.onCancel,  this.onConfirm,
+    required this.options,
+    this.onCancel,
+    this.onConfirm,
   });
   final String title;
   final String subtitle;
   final List<String> options;
-  final  VoidCallback? onCancel,onConfirm;
+  final VoidCallback? onCancel, onConfirm;
   @override
   State<CustomPopupList> createState() => _CustomPopupListState();
 }
 
 class _CustomPopupListState extends State<CustomPopupList> {
-  int? selectedIndex;
+  final Set<int> selectedIndexes = {};
   @override
-
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            widget.title,
-            style: context.textTheme.headLineSmallFont.copyWith(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 20),
-          Text(
-            widget.subtitle,
-            style: context.textTheme.labelLargeFont.copyWith(
-              color: AppConstantColors.gray200,
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          SizedBox(height: 20),
-
           Expanded(
-            child: ListView.separated(
-              itemCount: widget.options.length,
-              itemBuilder: (context, index) => SelectableCheckChip(
-                label: widget.options[index],
-                isSelected: selectedIndex == index,
-                onTap: () {
-                  setState(() {
-                    selectedIndex = index;
-                  });
-                },
-              ),
-              separatorBuilder: (BuildContext context, int index) =>
-                  SizedBox(height: 20),
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Text(
+                    widget.title,
+                    style: context.textTheme.headLineSmallFont.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+
+                const SliverToBoxAdapter(child: SizedBox(height: 20)),
+
+                SliverToBoxAdapter(
+                  child: Text(
+                    widget.subtitle,
+                    style: context.textTheme.labelLargeFont.copyWith(
+                      color: context.colors.gray500,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+
+                const SliverToBoxAdapter(child: SizedBox(height: 20)),
+
+                SliverList.separated(
+                  itemCount: widget.options.length,
+                  itemBuilder: (context, index) => SelectableCheckChip(
+                    label: widget.options[index],
+                    isSelected: selectedIndexes.contains(index),
+                    onTap: () {
+                      setState(() {
+                        if (selectedIndexes.contains(index)) {
+                          selectedIndexes.remove(index);
+                        } else {
+                          selectedIndexes.add(index);
+                        }
+                      });
+                    },
+                  ),
+                  separatorBuilder: (_, __) => const SizedBox(height: 20),
+                ),
+                const SliverToBoxAdapter(child: SizedBox(height: 90)),
+              ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: CustomPrimaryButton(
-                    // width: 200,
-                    buttonText: "cancel".tr(),
-                    buttonType: ButtonType.outlined,
-                    height: 52,
-                    borderRadius: 30,
-                    borderColor: AppConstantColors.purple900,
-                    foregroundColor: AppConstantColors.purple900,
-                    textStyle: context.textTheme.labelLargeFont,
-                    onPressed: widget.onCancel,
+          SafeArea(
+            top: false,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: CustomPrimaryButton(
+                      buttonText: "cancel".tr(),
+                      buttonType: ButtonType.outlined,
+                      height: 52,
+                      borderRadius: 30,
+                      borderColor: context.colors.purple900,
+                      foregroundColor: context.colors.purple900,
+                      textStyle: context.textTheme.labelLargeFont,
+                      onPressed: widget.onCancel,
+                    ),
                   ),
-                ),
-                SizedBox(width: 5),
-                Expanded(
-                  child: CustomPrimaryButton(
-                    // width: 200,
-                    buttonText: "select".tr(),
-                    buttonType: ButtonType.filled,
-                    height: 52,
-                    borderRadius: 30,
-                    backgroundColor: AppConstantColors.purple400,
-                    textStyle: context.textTheme.labelLargeFont,
-                    onPressed:widget.onConfirm,
+                  const SizedBox(width: 5),
+                  Expanded(
+                    child: CustomPrimaryButton(
+                      buttonText: "select".tr(),
+                      buttonType: ButtonType.filled,
+                      height: 52,
+                      borderRadius: 30,
+                      backgroundColor: context.colors.purple400,
+                      textStyle: context.textTheme.labelLargeFont,
+                      onPressed: widget.onConfirm,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
--- lib/core/design_system/components/popups/terms_popup.dart
+++ lib/core/design_system/components/popups/terms_popup.dart
@@ -1,82 +1,78 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hr_management/core/design_system/components/custom_primary_button.dart';
-import 'package:hr_management/core/design_system/theme/color/app_constant_colors.dart';
 import 'package:hr_management/core/design_system/theme/helper/theme_extention.dart';
 
-class CustomPopup extends StatelessWidget {
-  const CustomPopup({super.key});
+class TermsPopup extends StatelessWidget {
+  const TermsPopup({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            children: [
-              const SizedBox(height: 16),
-              Text(
-                "terms_and_conditions_title".tr(),
-                textAlign: TextAlign.center,
-                style: context.textTheme.titleLargeFontSemiBold.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: AppConstantColors.black,
-                  fontSize: 20,
-                ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          const SizedBox(height: 16),
+          Text(
+            "terms_and_conditions_title".tr(),
+            textAlign: TextAlign.center,
+            style: context.textTheme.titleLargeFontSemiBold.copyWith(
+              fontWeight: FontWeight.bold,
+              color: context.colors.black,
+              fontSize: 20,
+            ),
+          ),
+
+          const SizedBox(height: 20),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Color(0xffF9FAFB),
+                borderRadius: BorderRadius.circular(12),
               ),
+              child: Scrollbar(
+                thickness: 6.0,
+                thumbVisibility: true,
+                child: SingleChildScrollView(
+                  child: Text(
+                    "terms_and_conditions_desc".tr(),
 
-              const SizedBox(height: 20),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Color(0xffF9FAFB),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Scrollbar(
-                    thickness: 6.0,
-                    thumbVisibility: true,
-                    child: SingleChildScrollView(
-                      child: Text(
-                        "terms_and_conditions_desc".tr(),
-
-                        style: context.textTheme.labelMediumFont.copyWith(
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
+                    style: context.textTheme.labelMediumFont.copyWith(
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
               ),
-              const SizedBox(height: 20),
-              CustomPrimaryButton(
-                buttonText: "i_agree".tr(),
-                buttonType: ButtonType.gradient,
-                height: 52,
-                borderRadius: 30,
-                textStyle: context.textTheme.labelLargeFont,
-                gradient: const LinearGradient(
-                  colors: [
-                    AppConstantColors.purple500,
-                    AppConstantColors.purple800,
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-              ),
-              const SizedBox(height: 20),
-              CustomPrimaryButton(
-                buttonText: "decline".tr(),
-                buttonType: ButtonType.outlined,
-                height: 52,
-                borderRadius: 30,
-                borderColor: AppConstantColors.purple900,
-                foregroundColor: AppConstantColors.purple900,
-                textStyle: context.textTheme.labelLargeFont,
-                onPressed: () {},
-              ),
-              SizedBox(height: 10),
-            ],
+            ),
           ),
+          const SizedBox(height: 20),
+          CustomPrimaryButton(
+            buttonText: "i_agree".tr(),
+            buttonType: ButtonType.gradient,
+            height: 52,
+            borderRadius: 30,
+            textStyle: context.textTheme.labelLargeFont,
+            gradient: LinearGradient(
+              colors: [context.colors.purple500, context.colors.purple800],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          const SizedBox(height: 20),
+          CustomPrimaryButton(
+            buttonText: "decline".tr(),
+            buttonType: ButtonType.outlined,
+            height: 52,
+            borderRadius: 30,
+            borderColor: context.colors.purple900,
+            foregroundColor: context.colors.purple900,
+            textStyle: context.textTheme.labelLargeFont,
+            onPressed: () {},
+          ),
+          SizedBox(height: 10),
+        ],
+      ),
     );
   }
 }
