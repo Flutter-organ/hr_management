69f0ba69536327910085b42d09b6cc23e4399430
MohamedMahmoudAli <mohamed238ma@gmail.com>
2026-01-01 01:55:47 +0200

    add app ration

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppConfig {
+  AppConfig._();
+
+  static bool _isInitialized = false;
+  static Future<void> init() async {
+    if (_isInitialized) return;
+
+    await dotenv.load(fileName: '.env');
+    _isInitialized = true;
+  }
+
+  static void _ensureInitialized() {
+    if (!_isInitialized) {
+      throw Exception(
+        'AppConfig is not initialized. '
+        'Call AppConfig.init() before accessing configuration values.',
+      );
+    }
+  }
+
+
+  static String get apiBaseUrl {
+    _ensureInitialized();
+    return dotenv.env['API_BASE_URL'] ?? 'https://localhost:8080';
+  }
+
+  static String get appName {
+    _ensureInitialized();
+    return dotenv.env['APP_NAME'] ?? 'HR App';
+  }
+
+  static int get timeout {
+    _ensureInitialized();
+    final value = dotenv.env['TIMEOUT_SECONDS'];
+    return int.tryParse(value ?? '') ?? 10;
+  }
+  static String get environment {
+    _ensureInitialized();
+    return dotenv.env['ENV']?.toLowerCase() ?? 'development';
+  }
+  static bool get isProduction => environment == 'production';
+  static bool get isDevelopment => environment == 'development';
+  static bool get isStaging => environment == 'staging';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:hr_management/core/config/app_config.dart';
 import 'package:hr_management/core/design_system/theme/hr_management_theme.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await AppConfig.init();
 
   runApp(
     EasyLocalization(
