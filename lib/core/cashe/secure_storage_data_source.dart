49ed2b06ae6243140546e1ad16d6e8fb774b8322
Abd2lhakam19 <midoahme93@gmail.com>
2026-01-03 23:10:28 +0200

    refactor: update SecureDataSource interface and remove SecureRepository implementation

--- lib/core/cashe/secure_storage_data_source.dart
+++ lib/core/cashe/secure_storage_data_source.dart
@@ -1,39 +1,30 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-abstract class SecureDataSource {
-  Future<void> saveData({required String key, required String value});
-  Future<void> saveToken({required String token});
-  Future<String?> getData({required String key});
-  Future<String?> getToken();
-  Future<void> deleteData({required String key});
+abstract class SecureStorageService {
+  Future<void> write({required String key, required String value});
+  Future<String?> read({required String key});
+  Future<void> delete({required String key});
   Future<void> deleteAll();
 }
 
-class SecureStorageService implements SecureDataSource {
+class SecureStorageServiceImpl implements SecureStorageService {
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
   @override
-  Future<void> saveData({required String key, required String value}) async {
+  Future<void> write({required String key, required String value}) async {
     await _storage.write(key: key, value: value);
   }
 
-  @override
-  Future<void> saveToken({required String token}) async {
-    await _storage.write(key: 'user_token', value: token);
-  }
 
   @override
-  Future<String?> getData({required String key}) async {
+  Future<String?> read({required String key}) async {
     return await _storage.read(key: key);
   }
 
-  @override
-  Future<String?> getToken() async {
-    return await _storage.read(key: 'user_token');
-  }
+  
 
   @override
-  Future<void> deleteData({required String key}) async {
+  Future<void> delete({required String key}) async {
     await _storage.delete(key: key);
   }
 
