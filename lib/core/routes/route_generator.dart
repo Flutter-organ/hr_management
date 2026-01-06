00efd670a9e72155db11abbcd76f4344e73984ec
norhanadel367 <norhanadel367@gmail.com>
2026-01-06 02:19:31 +0200

    feat: implement route

---
+++ lib/core/routes/route_generator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hr_management/core/routes/route_names.dart';
+final GoRouter router = GoRouter(
+    routes: <RouteBase>[
+      // ───────────────────── Future / Commented Routes ─────────────────────
+      // Uncomment when ready to implement:
+      //
+      // GoRoute(
+      //   path: RouteNames.notifications,
+      //   name: RouteNames.notifications,
+      //   builder: (context, state) => const NotificationsScreen(),
+      // ),
+      ]
+);
---
+++ lib/core/routes/route_names.dart
@@ -0,0 +1,7 @@
+class RouteNames {
+  // ───────────────────── Future / Commented Routes ─────────────────────
+  // Uncomment when ready to implement:
+  // static const String namePageRoute = "namePage";
+  // static const String loginHomePageRoute = "/LoginHomePage";
+
+}
