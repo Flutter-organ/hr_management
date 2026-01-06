f04f52110fa676bef6759e3b0fcfaa04fbc07cba
norhanadel367 <norhanadel367@gmail.com>
2026-01-06 02:23:19 +0200

    feat: add router in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:hr_management/core/config/app_config.dart';
 import 'package:hr_management/core/design_system/theme/hr_management_theme.dart';
 
+import 'core/routes/route_generator.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
@@ -21,7 +23,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
@@ -30,27 +32,7 @@ class MyApp extends StatelessWidget {
       theme: HrManagementTheme.light(),
       darkTheme: HrManagementTheme.dark(),
       themeMode: ThemeMode.light,
-      home: const MyHomePage(title: 'HR Management Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        ],
-      ),
+      routerConfig: router,
     );
   }
 }
