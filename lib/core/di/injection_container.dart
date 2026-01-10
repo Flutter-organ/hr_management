d4e74934c76eecc83e60775343337f25513cf489
Amr Ashraf <AmrAshraf.info@gmail.com>
2026-01-10 14:46:19 +0200

    feat: add SharedPreferences integration for authentication flow

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../features/auth/data/data_source/local/auth_local_data_source.dart';
 import '../../features/auth/data/data_source/local/auth_local_data_source_imp.dart';
 import '../../features/auth/data/data_source/remote/AuthRemoteDataSource.dart';
@@ -10,6 +11,7 @@ import '../../features/auth/domain/usecases/ResetPasswordUseCase.dart';
 import '../../features/auth/presentation/logic/forget_password/forgot_password_cubit.dart';
 import '../../features/auth/presentation/logic/reset_password/ResetPasswordCubit.dart';
 import '../cache/secure_storage_data_source.dart';
+import '../cache/shared_preferences_service.dart';
 import '../network/dio_client.dart';
 
 final sl = GetIt.instance;
@@ -24,8 +26,11 @@ Future<void> _initCore() async {
         () => SecureStorageServiceImpl(),
   );
 
-  sl.registerLazySingleton<DioClient>(
-        () => DioClient(sl<AuthLocalDataSource>()),
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerSingleton<SharedPreferences>(sharedPreferences);
+
+  sl.registerLazySingleton<PreferencesService>(
+        () => SharedPreferencesServiceImpl(sl<SharedPreferences>()),
   );
 
 }
@@ -35,6 +40,9 @@ Future<void> _initAuth() async {
         () => AuthLocalDataSourceImp(
       secureStorageService: sl<SecureStorageService>(),
     ),
+  );
+  sl.registerLazySingleton<DioClient>(
+        () => DioClient(sl<AuthLocalDataSource>()),
   );
   sl.registerLazySingleton<AuthRemoteDataSource>(
         () => AuthRemoteDataSourceImp(dioClient: sl<DioClient>()),
