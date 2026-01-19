514a141b2b5ad2061fc3621c22917f1088ccdf9b
Amr Ashraf <AmrAshraf.info@gmail.com>
2026-01-19 03:50:11 +0200

    feat(auth): enhance authentication flow with secure storage and refactor related components

--- lib/features/auth/presentation/sign_up/widgets/build_signup_button.dart
+++ lib/features/auth/presentation/sign_up/widgets/build_signup_button.dart
@@ -1,8 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:iconsax/iconsax.dart';
-
 import '../../../../../core/design_system/components/custom_primary_button.dart';
 import '../../../../../core/design_system/theme/helper/extention_colors.dart';
 import '../../../../../core/design_system/theme/helper/theme_extention.dart';
